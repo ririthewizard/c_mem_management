ffaf65284444a4e70a896a983ba46e8aeed2954f
ririthewizard <ririwilliams.rw@gmail.com>
2025-02-16 16:49:51 -0600

    Figuring out new lsp server and also C basics

---
+++ c_basics/comments.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  /*
+    Sneklang is for nvim enjoyers
+    who want to write their own garbage
+    collectors instead of using off-the-shelf
+    solutions
+  */ 
+  printf("i use sneklang btw\n");
+  printf("i use nvim btw\n");
+  printf("i use arch btw\n");
+  return 0;
+}
---
+++ c_basics/types.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int max_recursive_calls = 100;
+    char io_mode = 'w';
+    float throttle_speed = 0.2;
+
+    printf("Max recursive calls: %d\n", max_recursive_calls);
+    printf("IO mode: %c\n", io_mode);
+    printf("Throttle speed: %f\n", throttle_speed);
+    printf("Hello world")
+    return 0;
+}
