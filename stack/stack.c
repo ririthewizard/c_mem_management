63c6550403ce3642f40e00378763e0c4234673c4
ririthewizard <ririwilliams.rw@gmail.com>
2025-03-01 12:41:18 -0600

    Implemented a simple stack with pop, push, and a free func for the whole stack

--- stack/stack.c
+++ stack/stack.c
@@ -3,8 +3,38 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-void stack_push(stack_t *stack, void *obj) {
+void stack_free(stack_t *stack) {
+    if (stack == NULL) {
+	return;
+    }
+    
+    if (stack->data != NULL) {
+	free(stack->data);
+    }
+    free(stack);
+}
 
+void *stack_pop(stack_t *stack) {
+    if (stack->count == 0) {
+	return NULL;
+    }
+    void *tmp = stack->data[stack->count - 1];
+    stack->count--;
+    return tmp;
+}
+
+void stack_push(stack_t *stack, void *obj) {
+    if (stack->count == stack->capacity) {
+	stack->capacity *= 2;
+	void *tmp_mem = realloc(stack->data, stack->capacity * sizeof(void *));
+	if (tmp_mem == NULL) {
+	    stack->capacity /= 2;
+	    return;
+	}
+	stack->data = tmp_mem;
+    }
+    stack->data[stack->count] = obj;
+    stack->count++;
 }
 
 stack_t *stack_new(size_t capacity) {
